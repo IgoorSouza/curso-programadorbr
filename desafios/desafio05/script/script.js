52bcc950fed41ac8a54fbbb7a625e65e8dbc7bb0
Igor Souza <94133842+IgoorSouza@users.noreply.github.com>
2023-09-01 17:21:31 -0300

    desafio 5

--- desafios/desafio05/script/script.js
+++ desafios/desafio05/script/script.js
@@ -2,7 +2,7 @@ function calc() {
   let inputAdulto = document.getElementsByTagName("input")[0]
   let inputCrianca = document.getElementsByTagName("input")[1]
   let inputDuracao = document.getElementsByTagName("input")[2]
-  let div = document.getElementById("resultado")
+  document.getElementById("resultado")
   let adulto = inputAdulto.value
   let crianca = inputCrianca.value
   let duracao = inputDuracao.value
@@ -11,14 +11,20 @@ function calc() {
   let bebida
 
   if (duracao <= 5) {
-    carne = adulto * 0.400 + crianca * 0.200
+    carne = (adulto * 400 + crianca * 200)/1000
     cerveja = Math.ceil(adulto * 1200/355) //355 = 1 lata de cerveja
     bebida = Math.ceil(adulto * 0.5 + crianca * 0.25)
   } else {
-    carne = adulto * 0.65 + crianca * 0.325
+    carne = (adulto * 650 + crianca * 325)/1000
     cerveja = Math.ceil(adulto * 2000 / 355)
     bebida = Math.ceil(adulto * 0.75 + crianca * 0.375)
   }
 
-  resultado.innerHTML = `<p>${carne} Kg de Carne</p> <p>${cerveja} Latas de Cerveja</p> <p>${bebida} Pet's 2l de Bebidas</p>` 
+  if (crianca == 0 && adulto == 0) {
+    resultado.innerHTML = "<p>Um churrasco sem pessoas? <br> Que estranho...</p>"
+  } else if (duracao == 0) {
+    resultado.innerHTML = "<p>Esse churrasco vai mesmo acontecer? Não consigo achar a duração dele...</p>"
+  } else {
+    resultado.innerHTML = `<p><span>${carne}</span> Kg de Carne</p> <p><span>${cerveja}</span> Latas de Cerveja</p> <p><span>${bebida}</span> Garrafa (s) de 2l de Refrigerante ou de Água</p>` 
+  }
 }
