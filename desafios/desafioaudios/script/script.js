f177086e79b0c18c6cbdee8206fc2c472b5b4c0e
Igor Souza <94133842+IgoorSouza@users.noreply.github.com>
2023-09-06 14:45:38 -0300

    update

--- desafios/desafioaudios/script/script.js
+++ desafios/desafioaudios/script/script.js
@@ -1,51 +1,47 @@
 var audio = document.getElementById("audio")
-    audio.volume = 0.2
+var texto = document.getElementById("texto")
+audio.volume = 0.2
 
-    function tocar() {
-      let confere = audio.currentTime
-      let pausado = audio.paused
-      let texto = document.getElementById("texto")
+function tocar() {
+  let tempo = audio.currentTime
+  let pausado = audio.paused
 
-      if (confere == 0) {
-        audio.currentTime = 1
-        audio.play()
-        texto.innerText = "Clique na imagem para pausar a música!"
-      } else if (confere != 0 && pausado == false) {
-        audio.pause()
-        texto.innerText = "Clique na imagem para retomar a música!"
-      } else if (confere != 0 && pausado == true) {
-        audio.play ()
-        texto.innerText = "Clique na imagem para pausar a música!"
-      }
-    }
-
-    function drop() {
-      let texto = document.getElementById("texto")
-
-      audio.currentTime = 122
-      audio.play()
-      texto.innerText = "Clique na imagem para pausar a música!"
-    }
-
-    function reiniciar() {
-      let confere = audio.currentTime
-      let texto = document.getElementById("texto")
-
-      if (confere != 0) {
-        audio.currentTime = 1
-        audio.play()
-        texto.innerText = "Clique na imagem para pausar a música!"
-      }
-    }
-
-    function pause() {
-      let confere = audio.currentTime
-      let texto = document.getElementById("texto")
-
+  if (tempo == 0) {
+    audio.currentTime = 1
+    audio.play()
+    texto.innerText = "Clique na imagem para pausar a música!"
+  } else if (tempo != 0 && pausado == false) {
       audio.pause()
-      if (confere == 0) {
-        texto.innerText = "Clique na imagem para tocar a música!"
-      } else {
-        texto.innerText = "Clique na imagem para retomar a música!"
-      }
-    }
+      texto.innerText = "Clique na imagem para retomar a música!"
+  } else if (tempo != 0 && pausado == true) {
+      audio.play ()
+      texto.innerText = "Clique na imagem para pausar a música!"
+  }
+}
+
+function drop() {
+  audio.currentTime = 122
+  audio.play()
+  texto.innerText = "Clique na imagem para pausar a música!"
+}
+
+function reiniciar() {
+  let tempo = audio.currentTime
+
+  if (tempo != 0) {
+    audio.currentTime = 1
+    audio.play()
+    texto.innerText = "Clique na imagem para pausar a música!"
+  }
+}
+
+function pause() {
+  let tempo = audio.currentTime
+
+  audio.pause()
+  if (tempo == 0) {
+    texto.innerText = "Clique na imagem para tocar a música!"
+  } else {
+    texto.innerText = "Clique na imagem para retomar a música!"
+  }
+}
