af19bdeef9cf5bc6e3ed2348d31295954d2fdfca
Igor Souza <94133842+IgoorSouza@users.noreply.github.com>
2023-09-27 10:04:01 -0300

    update style

---
+++ desafios/jogodamemoria/script/game.js
@@ -0,0 +1,1 @@
+let casas = document.getElementsByClassName("casa")
---
+++ desafios/jogodamemoria/script/interface.js
@@ -0,0 +1,6 @@
+let casas = document.getElementsByClassName("casa")
+
+function flipCard(casa) {
+  casa.setAttribute("class", "casa cartaVirada")
+  console.log(casa)
+}
