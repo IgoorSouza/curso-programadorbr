fcd46f1d088138e4ae2887128d6fb58fea2c7f74
Igor Souza <94133842+IgoorSouza@users.noreply.github.com>
2023-11-06 16:54:27 -0300

    mongoose

---
+++ desafios/links/controllers/linkController.js
@@ -0,0 +1,53 @@
+const Link = require('../models/Link')
+
+const redirect = async (req, res) => {
+  let title = req.params.title
+
+  try {
+    let doc = await Link.findOne({title})
+    if (doc){
+      res.redirect(doc.url)
+    } else {
+      next()
+    }
+    res.redirect(doc.url)
+  } catch(error) {
+    res.send(error)
+  }
+}
+
+const addLink = async (req, res) => {
+  let link = new Link(req.body)
+
+  try {
+    let doc = await link.save()
+    res.send("Link Adicionado com sucesso")
+  } catch (error) {
+    res.render('index', {error, body: req.body})
+  }
+}
+
+const allLinks = async(req, res) => {
+  try {
+    let links = await Link.find({})
+    res.render('all', {links})
+  } catch (error) {
+    res.send(error)
+  }
+}
+
+const deleteLink = async(req, res) => {
+  let id = req.params.id
+  if (!id) {
+    id = req.body.id
+  }
+
+  try {
+    await Link.findByIdAndDelete(id)
+    res.send(id)
+  } catch (error) {
+    res.send(error)
+  }
+}
+
+module.exports = {redirect, addLink, allLinks, deleteLink}
